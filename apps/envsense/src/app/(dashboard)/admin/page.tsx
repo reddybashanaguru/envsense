599bdccceddcac02d4d1ee6886b6941f4ae185df
Naguru Reddy Basha <nagurureddybasha@192.168.1.12>
2024-12-23 19:08:51 +0530

    added initial menu and sidebar

---
+++ apps/envsense/src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,26 @@
+// import Announcements from "@/components/Announcements";
+// import AttendanceChart from "@/components/AttendanceChart";
+// import CountChart from "@/components/CountChart";
+// import EventCalendar from "@/components/EventCalendar";
+// import FinanceChart from "@/components/FinanceChart";
+// import UserCard from "@/components/UserCard";
+
+const AdminPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+            Naguru Reddy Basha
+          {/* <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" /> */}
+        </div>
+    </div>
+    </div>
+  );
+};
+
+export default AdminPage;
